7387c14cf73f3c814f401b18c33163bdfe4e3fa3
kaanakyalcin <kaan.akyalcin@outlook.com>
2021-05-23 23:06:22 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { ProductComponent } from './components/product/product.component';
+import { TraceComponent } from './components/trace/trace.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    ProductComponent,
+    TraceComponent
   ],
   imports: [
     BrowserModule,
